d338cc1181cb96407c5c2ef70f52d7147c120b66
zhiyudong <yudong.zhi@chaitin.com>
2021-12-02 18:04:13 +0800

    test: success

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -10,9 +10,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: 'HomeP',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
---
+++ src/views/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="test-box">test page</div>
+</template>
+
+<script>
+import '@/styles/Test.less'
+
+export default {
+  name: 'TestPage',
+}
+</script>
